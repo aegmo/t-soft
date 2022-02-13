3dde4f61c3452b88e91e65cf3347b476f7c1c292
aegmo <yusufceyhun22@gmail.com>
2022-02-13 21:45:41 +0300

    Project Final

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,23 @@
-function myFunction() {
+function inputSearch() {
     var input, filter, listBox, category, a, i, txtValue;
     input = document.getElementById("categorySearch");
     filter = input.value.toUpperCase();
+    listBox = document.getElementById("listBox");
+    category = listBox.getElementsByClassName("col-lg-4");
+    for (i = 0; i < category.length; i++) {
+        a = category[i].getElementsByTagName("strong")[0];
+        txtValue = a.textContent || a.innerText;
+        if (txtValue.toUpperCase().indexOf(filter) > -1) {
+            category[i].style.display = "";
+        } else {
+            category[i].style.display = "none";
+        }
+    }
+}
+function selectChange() {
+    var select, filter, listBox, category, a, i, txtValue;
+    select = document.getElementById("categorySelect");
+    filter = select.value.toUpperCase();
     listBox = document.getElementById("listBox");
     category = listBox.getElementsByClassName("col-lg-4");
     for (i = 0; i < category.length; i++) {
